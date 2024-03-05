34127c3aaf895c9065755be1b8e399b2efbc6fb5
Sergio Freire <sergio.freire@gmail.com>
2024-03-05 16:46:15 +0000

    Revert "fixed gpg signing procedure"
    This reverts commit 309507570a074d5d03dfeeb385f7e6643f225b67.

--- publish_package.sh
+++ publish_package.sh
@@ -1,3 +1,3 @@
 # mvn --batch-mode clean compile test package gpg:sign deploy
-mvn clean compile test package javadoc:aggregate-jar gpg:sign deploy
+mvn clean compile test package javadoc:aggregate-jar deploy
 
