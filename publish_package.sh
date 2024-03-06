d78198c1e5c55d24db8c516e4aa1a151291ae154
Sergio Freire <sergio.freire@gmail.com>
2024-03-06 11:43:44 +0000

    add signing of package

--- publish_package.sh
+++ publish_package.sh
@@ -1,3 +1,2 @@
-# mvn --batch-mode clean compile test package gpg:sign deploy
-mvn clean compile test package javadoc:aggregate-jar deploy
+ mvn clean compile test package javadoc:aggregate-jar gpg:sign deploy
 
